384dcf7fcd3aa1b9a91702104f077f9a1f0d2421
Isaac Obuya <Szack4477@gmail.com>
2019-11-28 10:43:55 +0300

    update app with a shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component.jsx';
 
-const HatsPage = (props) => (
-  <div>
-      <h1>HATS PAGE</h1>
-      { console.log(props)};
-  </div>
-);
+// const HatsPage = (props) => (
+//   <div>
+//       <h1>HATS PAGE</h1>
+//       { console.log(props)};
+      
+//   </div>
+// );
 
 function App() {
   return (
     <div>
       <Switch>
         {/**Switch matches the exact path to render */}
+        {/** Route only passes the three props to the component specified and not to the children of the components such as Menu Item */}
       <Route exact component={HomePage} path={'/'} />
-      <Route component={HatsPage} path={'/hats'} />
+      <Route component={Shop} path={'/shop'} />
       </Switch>
     </div>
   );
