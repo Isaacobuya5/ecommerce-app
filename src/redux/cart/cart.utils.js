173eca5371543d94689a46baef33a319a2aec5fc
Isaac Obuya <Szack4477@gmail.com>
2019-12-30 11:49:42 +0300

    implement add and remove button on checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,3 +1,5 @@
+import CartItem from "../../components/cart-item/cart-item.component";
+
 // Utility functions helps us to keep our files clean nd organize functions that we ,ay need in multiple
 // files in one location.
 export const addItemToCart = (cartItems, cartItemToAdd) => {
@@ -14,4 +16,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  // first check if there exists cartItems
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
