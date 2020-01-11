0e60013bb54b6e4800a4e086c78cce3212f1d878
Isaac Obuya <Szack4477@gmail.com>
2020-01-11 18:30:34 +0300

    convert shop data from array to object for efficient queries

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,13 @@
 import { createSelector } from "reselect";
 
 // Matches the string value in the URL to their respective ID
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5
+// };
 
 const selectShop = state => state.shop;
 
@@ -16,10 +16,22 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+// create a new selector that helps convert our collection's object to an array
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
 // selectin different categories of items
+// export const selectCollection = collectionUrlParam =>
+//   createSelector([selectCollections], collections =>
+//     collections.find(
+//       collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+//     )
+//   );
+
 export const selectCollection = collectionUrlParam =>
-  createSelector([selectCollections], collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
   );
